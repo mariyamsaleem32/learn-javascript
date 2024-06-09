c3cedd94565b922bbf25ba216a7398bbb8394c82
mariyamsaleem32 <mariyamsaleem87@gmail.com>
2024-06-09 22:45:05 +0500

    add till 5 chapters

--- js one/chapters/script.js
+++ js one/chapters/script.js
@@ -1,5 +1,25 @@
+// ch varables for string 
+/*let fullName = "mariyam saleem";
+document.write("<h1>"+fullName+"</h1>");*/
+
+
 // ch varables for numbers 
 /* let num1 = 6;
  let num2 = 8;
  let result = num1+num2;
  console.log(result);*/
+
+
+ //Math expressions Familiar operators
+ //divition
+ alert(10/2);
+ //module
+  alert(10%3);
+
+ /*var originalNum = "23";
+var newNum = originalNum + 7;
+console.log(newNum);*/          //answer will be 237
+
+/*var originalNum = "23";
+var newNum = originalNum - 7;
+console.log(newNum); */         //now answer will be 16
