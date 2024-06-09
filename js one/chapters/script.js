3b32b544d963ad3f336c16cda0c56d602d9644f0
mariyamsaleem32 <mariyamsaleem87@gmail.com>
2024-06-10 03:02:54 +0500

    add till chapter 8

--- js one/chapters/script.js
+++ js one/chapters/script.js
@@ -10,11 +10,11 @@ document.write("<h1>"+fullName+"</h1>");*/
  console.log(result);*/
 
 
- //Math expressions Familiar operators
+ // ch Math expressions Familiar operators
  //divition
- alert(10/2);
+ //alert(10/2);
  //module
-  alert(10%3);
+//   alert(10%3);
 
  /*var originalNum = "23";
 var newNum = originalNum + 7;
@@ -23,3 +23,32 @@ console.log(newNum);*/          //answer will be 237
 /*var originalNum = "23";
 var newNum = originalNum - 7;
 console.log(newNum); */         //now answer will be 16
+
+
+//ch Math expressions: Unfamiliar operators
+/* 
+var num = 10;
+alert(num);*/
+/* num++; //increase 1
+ alert(num);*/
+ /*
+ num++;             //post
+ ++num;             //pre
+ num--;
+ alert(num);*/
+
+/*  var num =2 + 2 * 4 - 5 * 2;
+ alert(num);*/                //answer will be 0
+
+ 
+ /*var num =( 2 + 2) * 4 - 5 * 2;
+ alert(num); */               //now answer will be 6
+
+ /*let a = 2;
+ let b = 3;
+ let result = (a++ + --b) - b++ - --a * a++;
+ alert(result);*/            //answer will be -2
+
+
+ //ch Math expressions: Eliminating ambiguit
+console.log(((2+2)*2)-2*2);   //answer will be 4
