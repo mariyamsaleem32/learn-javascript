edf5b85065d438e22572305b0c24fc23998fe560
mariyamsaleem32 <mariyamsaleem87@gmail.com>
2024-06-15 23:52:11 +0500

    add till chapter 10

--- js one/chapters/script.js
+++ js one/chapters/script.js
@@ -1,6 +1,14 @@
 // ch varables for string 
-/*let fullName = "mariyam saleem";
-document.write("<h1>"+fullName+"</h1>");*/
+
+/*let firstName = "mariyam";
+let lastName = "saleem";
+let fullName = firstName + " " + lastName;
+document.write("fullName:"+fullName);*/
+
+//alert("student name"+ studentName); //it is not defined
+
+/*let studentName;
+console.log("student name"+ " " + studentName);*/ //it is undefined
 
 
 // ch varables for numbers 
@@ -9,12 +17,16 @@ document.write("<h1>"+fullName+"</h1>");*/
  let result = num1+num2;
  console.log(result);*/
 
+ //let currentNum = 6;
+ //let myNum = "20";
+ //currentNum = "hello world"; //change to string
+ //console.log("currentNum is "+currentNum);
+ //onsole.log(currentNum+10+Number(myNum)); //change to number answer 36
+
 
  // ch Math expressions Familiar operators
- //divition
- //alert(10/2);
- //module
-//   alert(10%3);
+// alert(10/2);     //divition
+// alert(10%3);   //module
 
  /*var originalNum = "23";
 var newNum = originalNum + 7;
@@ -44,13 +56,86 @@ alert(num);*/
  /*var num =( 2 + 2) * 4 - 5 * 2;
  alert(num); */               //now answer will be 6
 
+ 
  /*let a = 2;
  let b = 3;
  let result = (a++ + --b) - b++ - --a * a++;
- alert(result);*/            //answer will be -2
+              // 2 + 2 - 2 - 2 * 2
+              //  4 - 2 = 2   2 - 4 = -2
+ alert(result);        
+ //answer will be -2 
+ */
 
 
  //ch Math expressions: Eliminating ambiguity
-console.log(((2+2)*2)-2*2);   //answer will be 4
+//console.log( 5 * 2 - ((2 + 2)* 2) - 2 * 4);   //answer will be -6
+
+
+//Concatenating text strings
+/*
+ var message = "Thanks, ";
+ var userName = "mariyam"
+ var banger = "!";*/
+ //alert(message + userName + banger);
+ //we can also do 
+ /*var customMess = message + userName + banger;
+ alert(customMess);
+ */
+
+
+//if...else and else if statements
+
+/*
+var studentName = prompt("Enter your name");
+if (studentName.toLowerCase() == "mariyam") {
+alert("you are right user");
+}
+else if (studentName.toLowerCase() == "hina") {
+    alert("you are new user");
+}
+else{
+    alert("you are not right user");
+}*/
+
+/*
+var salary = amountThree;
+var amountOne = 10000;
+var amountTwo = 25000;
+var amountThree = 100000;
+var amountFour = 1000000;
+var amountFive = 10000000;*/
+//else condition will be work because I assigned variable to value before initializ and js works top to bottom.
+/*var amountOne = 10000;
+var amountTwo = 25000;
+var amountThree = 100000;
+var amountFour = 1000000;
+var amountFive = 10000000;
+var salary = amountThree;
+ // now it will work answer will be  you are average
+if (salary === amountOne) {
+    console.log("you are poor" + " " + studentName);
+} else if (salary === amountTwo) {
+    console.log("you are middle" + " " + studentName);
+} else if (salary === amountThree) {
+    console.log("you are average" + " " + studentName);
+} else if (salary === amountFour) {
+    console.log("you are rich" + " " + studentName);
+} else if (salary === amountFive) {
+    console.log("you are highly rich" + " " + studentName);
+} else {
+    console.log("I don't know about your status" + " " + studentName); 
+}*/
+
+/*
+var getNumber = prompt("Enter your number");
+console.log(typeof getNumber);  
+// answer is string becuse I do'nt use + or parseInt before prompt
+var getNumber = parseInt(prompt("Enter your number"));
+console.log(typeof getNumber);    //answer is number
+// you can also do it
+var getNumber = prompt("Enter your number");
+var getUserName = prompt("Enter your name");
+console.log(Number(getNumber) + typeof getUserName); 
+*/
 
 
